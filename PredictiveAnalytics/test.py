46d15399717693f604785a242ef6aefd241d6a26
KongWiKi <kongwiki@163.com>
2018-03-24 13:05:59 +0800

    upate

--- PredictiveAnalytics/test.py
+++ PredictiveAnalytics/test.py
@@ -1,7 +1,13 @@
 '''
 @author：KongWeiKun
 @file: test.py
-@time: 18-3-19 下午1:51
+@time: 18-3-24 上午9:54
 @contact: 836242657@qq.com
 '''
-import geopandas as geo
+import re
+data = '/home/kongweikun/PycharmProjects/Data_Mining/data/PA_chapter3_data/wine.names'
+pattern = re.compile('.*[0-9]+\)\s?(\w+).*')
+with open(data) as f:
+    for l in f:
+        if pattern.findall(l.strip()):
+            print(pattern.findall(l.strip())[0])
