b90fccc16f4e52108bd6babe20c2c840c9e7270f
KongWiKi <kongwiki@163.com>
2018-05-02 19:21:24 +0800

    更新

--- DMAlgorithms/DecisionTree/c4.5.py
+++ DMAlgorithms/DecisionTree/c4.5.py
@@ -4,3 +4,53 @@
 @time: 18-5-2 上午10:54
 @contact: kongwiki@163.com
 '''
+from math import log
+
+def createDataSet():
+    """
+    创建测试数据集
+    :return:dataSet: 数据集
+            labels: 分类属性
+    """
+    dataSet = [[0, 0, 0, 0, 'no'],  # 数据集
+               [0, 0, 0, 1, 'no'],
+               [0, 1, 0, 1, 'yes'],
+               [0, 1, 1, 0, 'yes'],
+               [0, 0, 0, 0, 'no'],
+               [1, 0, 0, 0, 'no'],
+               [1, 0, 0, 1, 'no'],
+               [1, 1, 1, 1, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [2, 0, 1, 2, 'yes'],
+               [2, 0, 1, 1, 'yes'],
+               [2, 1, 0, 1, 'yes'],
+               [2, 1, 0, 2, 'yes'],
+               [2, 0, 0, 0, 'no']]
+    labels = ['不放贷', '放贷']  # 分类属性
+    return dataSet, labels  # 返回数据集和分类属性
+
+def calcShannonEnt(dataSet):
+    """
+    计算给定数据集的经验熵
+    :param dataSet:数据集 
+    :return: shannonEnt:经验熵
+    """
+    numEntires = len(dataSet)  # 返回数据集的行数
+    labelCounts = {}  # 保存每个标签(Label)出现次数的字典
+    for featVec in dataSet:  # 对每组特征向量进行统计
+        currentLabel = featVec[-1]  # 提取标签(Label)信息
+        if currentLabel not in labelCounts.keys():  # 如果标签(Label)没有放入统计次数的字典,添加进去
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1  # Label计数
+    shannonEnt = 0.0  # 经验熵(香农熵)
+    for key in labelCounts:  # 计算香农熵
+        prob = float(labelCounts[key]) / numEntires  # 选择该标签(Label)的概率
+        shannonEnt -= prob * log(prob, 2)  # 利用公式计算
+    return shannonEnt  # 返回经验熵(香农熵)
+
+
+if __name__ == '__main__':
+    dataSet, features = createDataSet()
+    print(dataSet)
+    print(calcShannonEnt(dataSet))
