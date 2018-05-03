7109802d8e72563258f8f4803eb3e676f2cc5622
KongWiKi <kongwiki@163.com>
2018-05-03 12:26:37 +0800

    update

--- DMAlgorithms/DecisionTree/id3.py
+++ DMAlgorithms/DecisionTree/id3.py
@@ -1,122 +1,162 @@
 '''
 @author：KongWeiKun
 @file: id3.py
-@time: 18-5-2 上午11:14
+@time: 18-5-2 上午10:54
 @contact: kongwiki@163.com
 '''
-"""
-ID3
-"""
-from math import log2
+import operator
+from math import log
 
-# 定义节点类 二叉树
-class Node:
-    def __init__(self, root=True, label=None, feature_name=None, feature=None):
-        self.root = root
-        self.label = label
-        self.feature_name = feature_name
-        self.feature = feature
-        self.tree = {}
-        self.result = {'label:': self.label, 'feature': self.feature, 'tree': self.tree}
+def createDataSet():
+    """
+    创建测试数据集
+    :rtype:dataSet: 数据集
+            labels: 分类属性
+    """
+    dataSet = [[0, 0, 0, 0, 'no'],  # 数据集
+               [0, 0, 0, 1, 'no'],
+               [0, 1, 0, 1, 'yes'],
+               [0, 1, 1, 0, 'yes'],
+               [0, 0, 0, 0, 'no'],
+               [1, 0, 0, 0, 'no'],
+               [1, 0, 0, 1, 'no'],
+               [1, 1, 1, 1, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [2, 0, 1, 2, 'yes'],
+               [2, 0, 1, 1, 'yes'],
+               [2, 1, 0, 1, 'yes'],
+               [2, 1, 0, 2, 'yes'],
+               [2, 0, 0, 0, 'no']]
+    # labels = ['不放贷', '放贷']  # 分类属性
+    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']
+    return dataSet, labels  # 返回数据集和分类属性
 
-    def __repr__(self):
-        return '{}'.format(self.result)
+def calcShannonEnt(dataSet):
+    """
+    计算给定数据集的经验熵
+    :type dataSet:数据集 
+    :rtype: shannonEnt:经验熵
+    """
+    numEntires = len(dataSet)  # 返回数据集的行数
+    labelCounts = {}  # 保存每个标签(Label)出现次数的字典
+    for featVec in dataSet:  # 对每组特征向量进行统计
+        currentLabel = featVec[-1]  # 提取标签(Label)信息
+        if currentLabel not in labelCounts.keys():  # 如果标签(Label)没有放入统计次数的字典,添加进去
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1  # Label计数
+    shannonEnt = 0.0  # 经验熵(香农熵)
+    for key in labelCounts:  # 计算香农熵
+        prob = float(labelCounts[key]) / numEntires  # 选择该标签(Label)的概率
+        shannonEnt -= prob * log(prob, 2)  # 利用公式计算
+    return shannonEnt  # 返回经验熵(香农熵)
 
-    def add_node(self, val, node):
-        self.tree[val] = node
+def splitDataSet(dataSet, axis, value):
+    """
+    按照给定特征划分数据集
+    :type dataSet: 划分的数据集
+    :type axis: 划分数据集的特征
+    :type value: 需要返回的特征的值
+    :rtype: None
+    """
+    retDataSet = []                                        #创建返回的数据集列表
+    for featVec in dataSet:                             #遍历数据集
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]                #去掉axis特征
+            reducedFeatVec.extend(featVec[axis+1:])     #将符合条件的添加到返回的数据集
+            retDataSet.append(reducedFeatVec)
+    return retDataSet                                      #返回划分后的数据集
 
-    def predict(self, features):
-        if self.root is True:
-            return self.label
-        return self.tree[features[self.feature]].predict(features)
+def chooseBestFeatureToSplit(dataSet):
+    """
+    选择最优特征
+    :type dataSet: 数据集
+    :rtype: bestFeature: 信息增益最大的(最优)特征的索引值
+    """
+    numFeatures = len(dataSet[0]) - 1                    #特征数量
+    baseEntropy = calcShannonEnt(dataSet)                 #计算数据集的香农熵
+    bestInfoGain = 0.0                                  #信息增益
+    bestFeature = -1                                    #最优特征的索引值
+    for i in range(numFeatures):                         #遍历所有特征
+        #获取dataSet的第i个所有特征
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)                         #创建set集合{},元素不可重复
+        newEntropy = 0.0                                  #经验条件熵
+        for value in uniqueVals:                         #计算信息增益
+            subDataSet = splitDataSet(dataSet, i, value)         #subDataSet划分后的子集
+            prob = len(subDataSet) / float(len(dataSet))           #计算子集的概率
+            newEntropy += prob * calcShannonEnt(subDataSet)     #根据公式计算经验条件熵
+        infoGain = baseEntropy - newEntropy                     #信息增益
+        print("第%d个特征的增益为%.3f" % (i, infoGain))            #打印每个特征的信息增益
+        if (infoGain > bestInfoGain):                             #计算信息增益
+            bestInfoGain = infoGain                             #更新信息增益，找到最大的信息增益
+            bestFeature = i                                     #记录信息增益最大的特征的索引值
+    return bestFeature                                             #返回信息增益最大的特征的索引值
 
+def majorityCnt(classList):
+    """
+    统计classList中出现此处最多的元素(类标签)
+    :type classList: 类标签列表
+    :rtype:sortedClassCount[0][0]: 出现此处最多的元素(类标签)
+    """
+    classCount = {}
+    for vote in classList:                                        #统计classList中每个元素出现的次数
+        if vote not in classCount.keys():classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)        #根据字典的值降序排序
+    return sortedClassCount[0][0]
 
-class DTree:
-    def __init__(self, epsilon=0.1):
-        self.epsilon = epsilon
-        self._tree = {}
+def createTree(dataSet, labels, featLabels):
+    """
+    创建决策树
+    :type dataSet: 
+    :type labels: 
+    :type featLabels: 
+    :rtype: 
+    """
+    classList = [example[-1] for example in dataSet]            #取分类标签(是否放贷:yes or no)
+    if classList.count(classList[0]) == len(classList):            #如果类别完全相同则停止继续划分
+        return classList[0]
+    if len(dataSet[0]) == 1:                                    #遍历完所有特征时返回出现次数最多的类标签
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)                #选择最优特征
+    bestFeatLabel = labels[bestFeat]                            #最优特征的标签
+    featLabels.append(bestFeatLabel)
+    myTree = {bestFeatLabel:{}}                                    #根据最优特征的标签生成树
+    del(labels[bestFeat])                                        #删除已经使用特征标签
+    featValues = [example[bestFeat] for example in dataSet]        #得到训练集中所有最优特征的属性值
+    uniqueVals = set(featValues)                                #去掉重复的属性值
+    for value in uniqueVals:                                    #遍历特征，创建决策树。
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), labels, featLabels)
+    return myTree
 
-    # 熵
-    @staticmethod
-    def calc_ent(datasets):
-        data_length = len(datasets)
-        label_count = {}
-        for i in range(data_length):
-            label = datasets[i][-1]
-            if label not in label_count:
-                label_count[label] = 0
-            label_count[label] += 1
-        ent = -sum([(p / data_length) * log2(p / data_length) for p in label_count.values()])
-        return ent
+def classify(inputTree, featLabels, testVec):
+    """
+    使用决策树分类
+    :type inputTree: 已经生成的决策树
+    :type featLabels: 存储选择的最优特征标签
+    :type testVec: 测试数据列表，顺序对应最优特征标签
+    :rtype: classLabel: 分类结果
+    """
+    firstStr = next(iter(inputTree))                                                        #获取决策树结点
+    secondDict = inputTree[firstStr]                                                        #下一个字典
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else: classLabel = secondDict[key]
+    return classLabel
 
-    # 经验条件熵
-    def cond_ent(self, datasets, axis=0):
-        data_length = len(datasets)
-        feature_sets = {}
-        for i in range(data_length):
-            feature = datasets[i][axis]
-            if feature not in feature_sets:
-                feature_sets[feature] = []
-            feature_sets[feature].append(datasets[i])
-        cond_ent = sum([(len(p) / data_length) * self.calc_ent(p) for p in feature_sets.values()])
-        return cond_ent
-
-    # 信息增益
-    @staticmethod
-    def info_gain(ent, cond_ent):
-        return ent - cond_ent
-
-    def info_gain_train(self, datasets):
-        count = len(datasets[0]) - 1
-        ent = self.calc_ent(datasets)
-        best_feature = []
-        for c in range(count):
-            c_info_gain = self.info_gain(ent, self.cond_ent(datasets, axis=c))
-            best_feature.append((c, c_info_gain))
-        # 比较大小
-        best_ = max(best_feature, key=lambda x: x[-1])
-        return best_
-
-    def train(self, train_data):
-        """
-        input:数据集D(DataFrame格式)，特征集A，阈值eta
-        output:决策树T
-        """
-        _, y_train, features = train_data.iloc[:, :-1], train_data.iloc[:, -1], train_data.columns[:-1]
-        # 1,若D中实例属于同一类Ck，则T为单节点树，并将类Ck作为结点的类标记，返回T
-        if len(y_train.value_counts()) == 1:
-            return Node(root=True,
-                        label=y_train.iloc[0])
-
-        # 2, 若A为空，则T为单节点树，将D中实例树最大的类Ck作为该节点的类标记，返回T
-        if len(features) == 0:
-            return Node(root=True, label=y_train.value_counts().sort_values(ascending=False).index[0])
-
-        # 3,计算最大信息增益 同5.1,Ag为信息增益最大的特征
-        max_feature, max_info_gain = self.info_gain_train(np.array(train_data))
-        max_feature_name = features[max_feature]
-
-        # 4,Ag的信息增益小于阈值eta,则置T为单节点树，并将D中是实例数最大的类Ck作为该节点的类标记，返回T
-        if max_info_gain < self.epsilon:
-            return Node(root=True, label=y_train.value_counts().sort_values(ascending=False).index[0])
-
-        # 5,构建Ag子集
-        node_tree = Node(root=False, feature_name=max_feature_name, feature=max_feature)
-
-        feature_list = train_data[max_feature_name].value_counts().index
-        for f in feature_list:
-            sub_train_df = train_data.loc[train_data[max_feature_name] == f].drop([max_feature_name], axis=1)
-
-            # 6, 递归生成树
-            sub_tree = self.train(sub_train_df)
-            node_tree.add_node(f, sub_tree)
-
-        # pprint.pprint(node_tree.tree)
-        return node_tree
-
-    def fit(self, train_data):
-        self._tree = self.train(train_data)
-        return self._tree
-
-    def predict(self, X_test):
-        return self._tree.predict(X_test)
+if __name__ == '__main__':
+    dataSet, labels = createDataSet()
+    # print(calcShannonEnt(dataSet))
+    print("最优特征索引值:" + str(chooseBestFeatureToSplit(dataSet)))
+    featLabels = []
+    myTree = createTree(dataSet, labels, featLabels)
+    testVec = [0, 1]  # 测试数据
+    result = classify(myTree, featLabels, testVec)
+    if result == 'yes':
+        print('放贷')
+    if result == 'no':
+        print('不放贷')
